f90352bf845a548698642c8bdb7d4ac8db884229
HodaJahangiri <hoda.cs87@gmail.com>
2026-02-07 21:52:48 -0800

    install react-router-dom and defined routes for each page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
+import Order from "./pages/Order/Order";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="*" element={<NotFound/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/profile/update" element={<UpdateProfile/>}/>
+        <Route path="/orders" element={<Order/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
     </>
   )
 }
