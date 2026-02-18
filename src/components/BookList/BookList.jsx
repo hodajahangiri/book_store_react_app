5b24a0e0143f4dad4ed0543e67e9d44c7292ea74
HodaJahangiri <hoda.cs87@gmail.com>
2026-02-17 20:30:52 -0800

    Add Pagination to homePage.

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,18 +1,48 @@
-import BookCard from '../BookCard/BookCard'
+import BookCard from '../BookCard/BookCard';
+import Pagination from '@mui/material/Pagination';
+import { useState } from 'react';
 
-function BookList({ books, loading }) {
+
+function BookList({ books, loading, setStartIndex, IsSearch }) {
+
+  const [page, setPage] = useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    setStartIndex((value - 1) * 20);
+  }
 
   if (loading) return <div>Loading...</div>
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 m-10!'>
-      {books.length > 0 ?
-        books.map((book) => (
-          <BookCard key={book.isbn} book={book} />
-        ))
-        :
-        <h1> There is no Book Result </h1>
-      }
-    </div>
+    <>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 m-10!'>
+        {books.length > 0 ?
+          books.map((book) => (
+            <BookCard key={book.isbn} book={book} />
+          ))
+          :
+          <h1> There is no Book Result </h1>
+        }
+      </div>
+      <div className='flex justify-center my-5!'>
+        { IsSearch ? <></> :
+          <Pagination count={10} shape="rounded"
+            showFirstButton showLastButton
+            sx={{
+              '& .MuiPaginationItem-root.Mui-selected': {
+                backgroundColor: '#588157',
+                color: '#fff',
+                '&:hover': {
+                  backgroundColor: '#3a5a40',
+                },
+              },
+            }}
+            onChange={handleChange}
+            page={page}
+          />
+        }
+      </div>
+    </>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,56 +1,68 @@
 import SearchBar from '../../components/SearchBar/SearchBar';
 import BookList from '../../components/BookList/BookList';
 import LayoutPhoto from '../../components/LayoutPhoto/LayoutPhoto';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { API_GOOGLE_KEY } from '../../config';
+
 
 function Home({ loading, setLoading }) {
 
   const [books, setBooks] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [IsSearch, setIsSearch] = useState(false);
 
   const fetchBooks = async (title) => {
-    setLoading(true);
-    let url = ''
-    if (title) {
-      url = `https://www.googleapis.com/books/v1/volumes?q=${title}+intitle:keyes&maxResults=20&orderBy=relevance&key=${API_GOOGLE_KEY}`;
-    } else {
-      let randomIdx = Math.floor(Math.random() * 50);
-      url = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&startIndex=${randomIdx}&maxResults=20&key=${API_GOOGLE_KEY}`;
-    }
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const booksData = data.items?.map((book) => {
-        const volume = book.volumeInfo || {};
-        return {
-          title: volume.title,
-          subtitle: volume.subtitle || volume.title,
-          author: volume.authors ? volume.authors[0] : "Unknown",
-          publisher: volume.publisher || "Unknown",
-          published_date: volume.publishedDate || "Unknown",
-          description: volume.description || "",
-          isbn: book.id,
-          image_link: volume.imageLinks?.thumbnail?.replace("http://", "https://") || null,
-          language: volume.language,
-          averageRating: volume.averageRating || 0,
-          ratingsCount: volume.ratingsCount || 5,
-          page_count: volume.pageCount
-        };
-      }) || [];
-      setBooks(booksData);
-    } catch (error) {
-      console.error("Error fetching books:", error);
-    }
-    setLoading(false);
-  };
+      setLoading(true);
+      let url = ''
+      if (title) {
+        setIsSearch(true);
+        url = `https://www.googleapis.com/books/v1/volumes?q=${title}+intitle:keyes&maxResults=20&orderBy=relevance&key=${API_GOOGLE_KEY}`;
+      } else {
+        setIsSearch(false);
+        let randomIdx = Math.floor(Math.random() * 50);
+        url = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&startIndex=${startIndex}&maxResults=20&key=${API_GOOGLE_KEY}`;
+      }
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const booksData = data.items?.map((book) => {
+          const volume = book.volumeInfo || {};
+          return {
+            title: volume.title,
+            subtitle: volume.subtitle || volume.title,
+            author: volume.authors ? volume.authors[0] : "Unknown",
+            publisher: volume.publisher || "Unknown",
+            published_date: volume.publishedDate || "Unknown",
+            description: volume.description || "",
+            isbn: book.id,
+            image_link: volume.imageLinks?.thumbnail?.replace("http://", "https://") || null,
+            language: volume.language,
+            averageRating: volume.averageRating || 0,
+            ratingsCount: volume.ratingsCount || 5,
+            page_count: volume.pageCount
+          };
+        }) || [];
+        setBooks(booksData);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+      setLoading(false);
+    };
+
+    useEffect(() => {
+      const getBooks = async() => {
+        fetchBooks();
+      }
+      getBooks()
+  }, [startIndex])
 
   return (
     <>
       <LayoutPhoto />
       <div className="flex justify-center py-5! w-full border-b-black border-b-2">
-        <SearchBar fetchBooks={fetchBooks} />
+        <SearchBar fetchBooks={fetchBooks}/>
       </div>
-      {books && <BookList books={books} loading={loading} />}
+      {books && <BookList books={books} loading={loading} setStartIndex={setStartIndex} IsSearch={IsSearch} />}
     </>
   )
 }
