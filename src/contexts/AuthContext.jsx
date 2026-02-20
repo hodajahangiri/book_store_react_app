ae523fd5fdc1b33800064922928efafd2c0e953d
HodaJahangiri <hoda.cs87@gmail.com>
2026-02-20 11:17:57 -0800

    change the delete user in authContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -96,30 +96,28 @@ export const AuthProvider = ({ children }) => {
 
     //deleteUser function
     const deleteUser = async () => {
-        toast.success("Your Account successfully deleted.");
-            logout();
-        // try {
-        //     const response = await fetch(`${API_BASE_URL}users`, {
-        //         method: "DELETE",
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': 'Bearer ' + token
-        //         }
-        //     })
-        //     const responseData = await response.json();
-        //     if (response.ok) {
-        //         toast.success(responseData.message);
-        //         logout();
-        //     } else if (response.status === 403) {
-        //         const responseData = await response.json();
-        //         toast.info(`${responseData.message}, You have to log in again`);
-        //         logout();
-        //     } else {
-        //         console.warn(responseData.message);
-        //     }
-        // } catch (error) {
-        //     toast.error(`Error: ${error}`);
-        // }
+        try {
+            const response = await fetch(`${API_BASE_URL}users`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+            })
+            const responseData = await response.json();
+            if (response.ok) {
+                toast.success(responseData.message);
+                logout();
+            } else if (response.status === 403) {
+                const responseData = await response.json();
+                toast.info(`${responseData.message}, You have to log in again`);
+                logout();
+            } else {
+                console.warn(responseData.message);
+            }
+        } catch (error) {
+            toast.error(`Error: ${error}`);
+        }
     };
 
     // UpdateProfile function
